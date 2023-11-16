b45c5e9875f45d0c5f88534d684b0fc8808fff52
imovrhro <danylo.malovichko@gmail.com>
2023-11-16 22:32:17 +0200

    iOS 17 fix

--- MyProfile/Extensions/Color.swift
+++ MyProfile/Extensions/Color.swift
@@ -12,7 +12,7 @@ extension Color {
     static let regularPurple = Color("CustomPurple")
     static let darkerPurple = Color("CustomDarkerPurple")
     static let backgroundColor = Color("BackgroundColor")
-    static let greySeparator = Color("GreySeparatorColor")
+    static let greySeparatorColor = Color("GreySeparatorColor")
     
     init(hex: String) {
         let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
